e1bae9784498beb51df840f8fe8cdcdba1482e71
Manuel Alejandro Rios <malejandrorios@javeriana.edu.co>
2022-03-13 14:41:52 -0500

    Creacion Base del Proyecto con Spring
    Se agrega lo creado del proyecto con Spring

---
+++ Proyecto Restaurantes/src/test/java/co/javeriana/restaurantes/RestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.javeriana.restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
