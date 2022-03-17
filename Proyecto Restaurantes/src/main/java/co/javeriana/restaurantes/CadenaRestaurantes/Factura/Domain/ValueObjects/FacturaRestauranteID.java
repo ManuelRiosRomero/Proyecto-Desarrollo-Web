252fa1b6606c7bb5fa83cb77691539ee6a6c1160
Manuel Alejandro Rios <malejandrorios@javeriana.edu.co>
2022-03-17 14:45:58 -0500

    Completar Entrega de Java 1
    Se completo lo faltante para terminar la primera entrega del proyecto en Codigo.

--- Proyecto Restaurantes/src/main/java/co/javeriana/restaurantes/CadenaRestaurantes/Factura/Domain/ValueObjects/FacturaRestauranteID.java
+++ Proyecto Restaurantes/src/main/java/co/javeriana/restaurantes/CadenaRestaurantes/Factura/Domain/ValueObjects/FacturaRestauranteID.java
@@ -1,4 +1,12 @@
 package co.javeriana.restaurantes.CadenaRestaurantes.Factura.Domain.ValueObjects;
 
-public class FacturaRestauranteID {
+import co.javeriana.restaurantes.Shared.Domain.Aggregate.CustomUUID;
+
+public class FacturaRestauranteID extends CustomUUID {
+
+    private FacturaRestauranteID() {}
+
+    public FacturaRestauranteID(String value) {
+        super(value);
+    }
 }
